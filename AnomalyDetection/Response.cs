2536a13baaac06911c5f6532c8210668399a36f2
Chenjian Liang <chliang@microsoft.com>
2018-05-07 11:32:31 +0800

    Change response.

--- AnomalyDetection/Response.cs
+++ AnomalyDetection/Response.cs
@@ -6,19 +6,15 @@ using System.Threading.Tasks;
 
 namespace AnomalyDetection
 {
-    public class Result
+    public class Response
     {
+        public double? Period { get; set; }
+        public string WarningText { get; set; }
         public List<double> ExpectedValue { get; set; }
         public List<double> UpperMargin { get; set; }
         public List<double> LowerMargin { get; set; }
         public List<bool> IsAnomaly { get; set; }
         public List<bool> IsAnomaly_Neg { get; set; }
         public List<bool> IsAnomaly_Pos { get; set; }
-    }
-
-    public class Response
-    {
-        public double? Period { get; set; }
-        public Result Results { get; set; }
     }
 }
